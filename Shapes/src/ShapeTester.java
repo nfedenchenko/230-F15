e234643d3908393dbd51f264edbd62b6a1b26da1
Nicholas Erwin <nerwin@drew.edu>
2015-09-22 18:31:08 -0400

    double sideLength now initializes to 3.0

--- Shapes/src/ShapeTester.java
+++ Shapes/src/ShapeTester.java
@@ -16,7 +16,6 @@ public class ShapeTester {
 		
 		for (Shape shape : shapes) {
 			String s = 	"------------------------------------- \n";
-			shape.setSideLength(5);
 			s +=			"Side Length: " + shape.getSideLength()+"\n";
 			s +=			"Num sides:   " + shape.getNumSides()+"\n";
 			s += 	 	"Area: 		  " + shape.getArea()+"\n";
@@ -24,6 +23,7 @@ public class ShapeTester {
 			s +=			"Height:		  " + shape.getHeight()+"\n";
 			s +=			"Width:		  " + shape.getWidth()+"\n";
 			s +=			"Angle:		  " + shape.getInteriorAngle()+"\n";
+			shape.setSideLength(5);
 			s +=			"New Length:  " + shape.getSideLength()+"\n";
 			s +=			"-------------------------------------";
 			System.out.println(s);
